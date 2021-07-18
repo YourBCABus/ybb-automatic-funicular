496c7596ec81442308e256e93edb28de7f427876
Anthony Li <me@anli.dev>
2021-07-17 23:13:41 -0400

    Send notifications only for readable schools

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import redis
 import json
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 def main():
     r = redis.Redis(
@@ -9,13 +11,17 @@ def main():
         db=int(os.getenv("REDIS_DB", "0")),
     )
 
+    db = MongoClient(os.getenv("DATABASE_URL")).yourbcabus
+
     pubsub = r.pubsub()
     pubsub.subscribe("BUS_BOARDING_AREA_CHANGE")
 
     for message in pubsub.listen():
         if message["type"] == "message":
             data = json.loads(message["data"])
-            print(data)
+            school = db.schools.find_one({"_id": ObjectId(data["schoolID"])})
+            if school and school["public_scopes"] and "read" in school["public_scopes"]:
+                print("Sending notification")
 
 if __name__ == "__main__":
     main()
