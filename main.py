8d62cc7992c3aee9dd8c26e48f3ee42f99beffaf
Anthony Li <me@anli.dev>
2021-08-19 17:38:33 -0400

    Add Firebase

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import redis
 import json
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import firebase_admin
+from firebase_admin import credentials, messaging
+import datetime
+
+firebase_admin.initialize_app(credentials.Certificate("serviceaccountkey.json"))
 
 def main():
     r = redis.Redis(
@@ -20,8 +25,29 @@ def main():
         if message["type"] == "message":
             data = json.loads(message["data"])
             school = db.schools.find_one({"_id": ObjectId(data["schoolID"])})
-            if school and school["public_scopes"] and "read" in school["public_scopes"]:
-                print("Sending notification")
+            # if school and school["public_scopes"] and "read" in school["public_scopes"]:
+            if school:
+                now = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+                school_id = data["schoolID"]
+                bus_id = data["busID"]
+                bus_name = data["busName"]
+                boarding_area = data["newBoardingArea"]
+                message = messaging.Message(
+                    data={
+                        "bus": bus_id,
+                        "boarding_area": boarding_area,
+                        "location": boarding_area,
+                        "invalidate_time": data["invalidateTime"],
+                        "time": now
+                    },
+                    notification=messaging.Notification(
+                        title=f"{bus_name} boarding at {boarding_area}",
+                        body=f"The bus for \"{bus_name}\" is now at {boarding_area}."
+                    ),
+                    apns=messaging.APNSConfig(payload=messaging.APNSPayload(messaging.Aps(sound="default"))),
+                    topic=f"school.{school_id}.bus.{bus_id}"
+                )
+                messaging.send(message)
 
 if __name__ == "__main__":
     main()
